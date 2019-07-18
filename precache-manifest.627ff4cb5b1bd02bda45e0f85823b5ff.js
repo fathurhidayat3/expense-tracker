b458952221669c9b894418cfe33c436a1fa72526
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-18 23:52:00 +0700

    Updates

---
+++ precache-manifest.627ff4cb5b1bd02bda45e0f85823b5ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e0491e792698f43801a05e15cfff3cd",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "66ca6a788ef7a8682039",
+    "url": "/expense-tracker/static/css/main.7c462ee7.chunk.css"
+  },
+  {
+    "revision": "1b0dd42482f4ae0d2442",
+    "url": "/expense-tracker/static/js/2.b7086df0.chunk.js"
+  },
+  {
+    "revision": "66ca6a788ef7a8682039",
+    "url": "/expense-tracker/static/js/main.0d78f026.chunk.js"
+  },
+  {
+    "revision": "5baca6083ea1223d23cf",
+    "url": "/expense-tracker/static/js/runtime~main.ae18d0c9.js"
+  }
+]);
