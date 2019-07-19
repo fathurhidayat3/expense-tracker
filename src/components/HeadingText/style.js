162f6f2f7361e584cece0bdccd6dfb12182ba7c8
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-19 14:28:35 +0700

    Add HeadingText component

---
+++ src/components/HeadingText/style.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const HeadingTextH1 = styled.h1`
+  margin: 8px 0 0 0;
+
+  font-family: "Rubik", sans-serif;
+  font-weight: bold;
+`;
+
+const HeadingTextH2 = styled.h2`
+  margin: 6px 0 0 0;
+
+  font-family: "Rubik", sans-serif;
+  font-weight: bold;
+`;
+
+const HeadingTextH3 = styled.h3`
+  margin: 4px 0 0 0;
+
+  font-family: "Rubik", sans-serif;
+  font-weight: bold;
+`;
+
+const HeadingTextH4 = styled.h4`
+  margin: 3px 0 0 0;
+
+  font-family: "Rubik", sans-serif;
+  font-weight: bold;
+`;
+
+export { HeadingTextH1, HeadingTextH2, HeadingTextH3, HeadingTextH4 };
