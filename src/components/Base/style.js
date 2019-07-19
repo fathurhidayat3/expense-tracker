80d0757cec78b3bacd4f77fc6c139e99874a97bb
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-19 14:30:04 +0700

    Modify Base component

--- src/components/Base/style.js
+++ src/components/Base/style.js
@@ -1,14 +1,18 @@
 import styled from "styled-components";
 
+import { color, layout } from "../../constants";
+
+const { spacing } = layout;
+
 const Base = styled.div`
   flex: 1;
 
-  padding: 16px;
+  padding: ${spacing.lg};
 
   height: 100%;
   width: 360px;
 
-  background: #ffffff;
+  background: ${color.gray};
 `;
 
 export { Base };
