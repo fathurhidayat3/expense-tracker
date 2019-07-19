2f2194cd42e02cc47161829f4cef883332325e10
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-19 14:25:15 +0700

    Add color and layout constants

---
+++ src/constants/color.js
@@ -0,0 +1,9 @@
+export default {
+  primary: "#ff3378",
+  secondary: "#33c9ff",
+  optional: "#41e94b",
+  light: "#ffffff",
+  gray: "#fbfbfb",
+  grayDarker: "#efefef",
+  grayDarker2: "#a7a9aa"
+};
---
+++ src/constants/layout.js
@@ -0,0 +1,7 @@
+export default {
+  spacing: {
+    sm: "4px",
+    md: "8px",
+    lg: "16px"
+  }
+};
