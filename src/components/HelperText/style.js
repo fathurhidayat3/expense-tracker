ad1e490ec1bf6a8320d53c768a5fca7d77092f19
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-19 14:28:49 +0700

    Add HelperText component

---
+++ src/components/HelperText/style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+import { color } from "../../constants";
+
+const HelperText = styled.h6`
+  margin: 2px 0 0 0;
+
+  font-family: "Rubik", sans-serif;
+  font-weight: bold;
+
+  color: ${color.grayDarker2};
+`;
+
+export { HelperText };
