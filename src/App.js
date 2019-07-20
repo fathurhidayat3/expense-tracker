3f01291465a89d19d42c83337a2a65ef737924fd
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-20 11:24:27 +0700

    Update App with top and bottom nav

--- src/App.js
+++ src/App.js
@@ -1,62 +1,102 @@
 import React from "react";
-
+import { Search } from "react-feather";
 import Base from "./components/Base";
-import FlexWrapper from "./components/FlexWrapper";
+import BottomNavbar from "./components/BottomNavbar";
+import ButtonIcon from "./components/ButtonIcon";
 import {
+  CardBottom,
   CardContainer,
-  CardWrapper,
   CardTop,
-  CardBottom
+  CardWrapper
 } from "./components/Card";
+import FlexWrapper from "./components/FlexWrapper";
 import HeadingText from "./components/HeadingText";
-import { HelperText } from "./components/HelperText";
+import HelperText from "./components/HelperText";
+import TopNavbar from "./components/TopNavbar/style";
 
 function App() {
   return (
     <Base>
-      <FlexWrapper direction="row">
-        <CardWrapper>
-          <CardContainer>
-            <CardTop>
-              <img
-                src="/images/icons/previous.svg"
-                alt="et-logo"
-                style={{
-                  height: 25,
-                  width: 25,
-                  objectFit: "cover",
-                  borderRadius: 25
-                }}
-              />
-            </CardTop>
-            <CardBottom>
-              <HelperText>Income</HelperText>
-              <HeadingText type="h4">$6,593.75</HeadingText>
-            </CardBottom>
-          </CardContainer>
-        </CardWrapper>
+      <TopNavbar>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <HeadingText type="h3">Stat</HeadingText>
 
-        <CardWrapper>
-          <CardContainer>
-            <CardTop>
-              <img
-                src="/images/icons/play-button.svg"
-                alt="et-logo"
-                style={{
-                  height: 25,
-                  width: 25,
-                  objectFit: "cover",
-                  borderRadius: 25
-                }}
-              />
-            </CardTop>
-            <CardBottom>
-              <HelperText>Expense</HelperText>
-              <HeadingText type="h4">$2,645.50</HeadingText>
-            </CardBottom>
-          </CardContainer>
-        </CardWrapper>
-      </FlexWrapper>
+          <ButtonIcon>
+            <Search size={15} />
+          </ButtonIcon>
+        </div>
+      </TopNavbar>
+
+      <div style={{ padding: "8px 0 24px 0", overflowY: "scroll", zIndex: 1 }}>
+        {[0, 0, 0, 0].map((item, index) => (
+          <FlexWrapper direction="row" key={index}>
+            <CardWrapper>
+              <CardContainer>
+                <CardTop>
+                  <img
+                    src="/images/icons/previous.svg"
+                    alt="et-logo"
+                    style={{
+                      height: 25,
+                      width: 25,
+                      objectFit: "cover",
+                      borderRadius: 25
+                    }}
+                  />
+                </CardTop>
+                <CardBottom>
+                  <HelperText>Income</HelperText>
+                  <HeadingText type="h4">$6,593.75</HeadingText>
+                </CardBottom>
+              </CardContainer>
+            </CardWrapper>
+
+            <CardWrapper>
+              <CardContainer>
+                <CardTop>
+                  <img
+                    src="/images/icons/play-button.svg"
+                    alt="et-logo"
+                    style={{
+                      height: 25,
+                      width: 25,
+                      objectFit: "cover",
+                      borderRadius: 25
+                    }}
+                  />
+                </CardTop>
+                <CardBottom>
+                  <HelperText>Expense</HelperText>
+                  <HeadingText type="h4">$2,645.50</HeadingText>
+                </CardBottom>
+              </CardContainer>
+            </CardWrapper>
+          </FlexWrapper>
+        ))}
+      </div>
+
+      <BottomNavbar
+        leftNav={[
+          {
+            icon: "Calendar",
+            text: "Daily"
+          },
+          {
+            icon: "BarChart2",
+            text: "Stat"
+          }
+        ]}
+        rightNav={[
+          {
+            icon: "Book",
+            text: "Budget"
+          },
+          {
+            icon: "User",
+            text: "Profile"
+          }
+        ]}
+      />
     </Base>
   );
 }
