b6c9c502f8c4b6412461c6c430f3e708342df269
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-20 16:31:31 +0700

    Add ListGroup component

---
+++ src/components/ListGroup/index.js
@@ -0,0 +1,1 @@
+export { ListGroup, ListGroupItem } from "./style";
---
+++ src/components/ListGroup/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const ListGroup = styled.ul`
+  list-style: none;
+
+  margin: 0 8px;
+  padding: 0;
+
+  background: red;
+`;
+
+const ListGroupItem = styled.li`
+  margin: 0;
+  padding: 0;
+
+  background: blue;
+`;
+
+export { ListGroup, ListGroupItem };
