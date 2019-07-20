f5c684f11c266b90f7a8eaacfd4884224bf143e4
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-20 14:08:43 +0700

    Apply flow and refactor

--- src/components/ButtonIcon/index.js
+++ src/components/ButtonIcon/index.js
@@ -1,11 +1,30 @@
-import React from "react";
+// @flow
+
+import * as React from "react";
+import * as Icon from "react-feather";
+
 import { ButtonIconStyled } from "./style";
 import HelperText from "../HelperText";
 
-const ButtonIcon = ({ children, withText, ...otherProps }) => {
+type IconProp = {
+  name: string,
+  size: number
+};
+
+type Props = {
+  children?: React.Node,
+  icon?: IconProp,
+  withText?: string,
+  color?: String,
+  otherProps?: any
+};
+
+const ButtonIcon = ({ children, icon, withText, ...otherProps }: Props) => {
+  const TempIcon = Icon[icon && icon.name];
+
   return (
     <ButtonIconStyled withText={withText} {...otherProps}>
-      {children}
+      <TempIcon size={15} />
       <HelperText style={{ marginTop: 8 }}>{withText}</HelperText>
     </ButtonIconStyled>
   );
