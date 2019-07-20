7b7bd7ce19847b2dedcb9f6232541ec46488fd2c
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-20 11:23:03 +0700

    Add FlexWrapper component

--- src/components/FlexWrapper/style.js
+++ src/components/FlexWrapper/style.js
@@ -5,6 +5,10 @@ const FlexWrapper = styled.div`
   display: flex;
   flex-direction: ${props => (props.direction ? props.direction : "row")};
   flex-wrap: ${props => props.wrap && "wrap"};
+  justify-content: ${props => props.justifyContent && props.justifyContent};
+  align-items: ${props => props.alignItems && props.alignItems};
+
+  padding: ${props => (props.noPadding ? "0" : "0 16px")};
 `;
 
 export { FlexWrapper };
