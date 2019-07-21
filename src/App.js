63d352dbdd5c1bb3b4ab2eb0a0d885ca97c9dda7
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-21 19:04:38 +0700

    Populate Daily with dummy, connect with context

--- src/App.js
+++ src/App.js
@@ -1,87 +1,99 @@
 // @flow
 
+import dayjs from "dayjs";
 import * as React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Daily from "./pages/Daily";
-import Stat from "./pages/Stat";
 import Base from "./components/Base";
-import DateScroller from "./components/DateScroller";
-
 import BottomNavbar from "./components/BottomNavbar";
 import ButtonIcon from "./components/ButtonIcon";
+import DateScroller from "./components/DateScroller";
 import HeadingText from "./components/HeadingText";
 import TopNavbar from "./components/TopNavbar/style";
-import HelperText from "./components/HelperText";
 import { color } from "./constants";
+import DateContext from "./contexts/DateContext";
+import Daily from "./pages/Daily";
+import Stat from "./pages/Stat";
 
 function App() {
+  const [etDate, setEtDate] = React.useState(dayjs().format("YYYY-MM-DD"));
+
   return (
     <Router>
-      <Base>
-        <TopNavbar>
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <HeadingText type="h3">Daily transaction</HeadingText>
+      <DateContext.Provider value={{ etDate: etDate, setEtDate: setEtDate }}>
+        <Base>
+          <TopNavbar>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <HeadingText type="h3">Daily transaction</HeadingText>
 
-            <ButtonIcon
-              icon={{ name: "Search", size: 15, color: color.dark }}
+              <ButtonIcon
+                icon={{ name: "Search", size: 15, color: color.dark }}
+              />
+            </div>
+
+            <DateScroller
+              defaultDate={("0" + etDate).slice(-2)}
+              data={Array.from(
+                new Array(dayjs(etDate).daysInMonth())
+                  .fill()
+                  .map((e, index) => {
+                    return {
+                      num: ("0" + (index + 1)).slice(-2),
+                      day: dayjs(etDate.substr(0, 8) + (index + 1)).format(
+                        "ddd"
+                      )
+                    };
+                  })
+              )}
+              buttonStyle={{
+                activeBackground: "red",
+                activeColor: "yellow"
+              }}
+              onButtonClick={data => setEtDate(etDate.substr(0, 8) + data.num)}
             />
-          </div>
+          </TopNavbar>
 
-          <DateScroller
-            data={[
-              { num: 1, day: "Sun", isActive: false },
-              { num: 2, day: "Mon", isActive: true },
-              { num: 3, day: "Tue", isActive: false },
-              { num: 4, day: "Wed", isActive: false },
-              { num: 5, day: "Thu", isActive: false },
-              { num: 6, day: "Fri", isActive: false },
-              { num: 7, day: "Sat", isActive: false },
-              { num: 8, day: "Sun", isActive: false },
-              { num: 9, day: "Mon", isActive: false },
-              { num: 10, day: "Tue", isActive: false },
-              { num: 11, day: "Wed", isActive: false }
+          <Route exact path="/" component={() => <Daily />} />
+          <Route path="/stat" component={() => <Stat />} />
+          <Route path="/budget" component={() => "budget"} />
+          <Route path="/profile" component={() => "profile"} />
+
+          <BottomNavbar
+            defaultPath={"/"}
+            leftNav={[
+              {
+                icon: {
+                  name: "Calendar",
+                  size: 15,
+                  color: color.grayDarker2
+                },
+                text: "Daily",
+                pathname: "/"
+              },
+              {
+                icon: {
+                  name: "BarChart2",
+                  size: 15,
+                  color: color.grayDarker2
+                },
+                text: "Stat",
+                pathname: "/stat"
+              }
             ]}
-            buttonStyle={{
-              activeBackground: "red",
-              activeColor: "yellow"
-            }}
-            onButtonClick={data => console.log(data)}
+            rightNav={[
+              {
+                icon: { name: "Book", size: 15, color: color.grayDarker2 },
+                text: "Budget",
+                pathname: "/budget"
+              },
+              {
+                icon: { name: "User", size: 15, color: color.grayDarker2 },
+                text: "Profile",
+                pathname: "/profile"
+              }
+            ]}
           />
-        </TopNavbar>
-
-        <Route exact path="/" component={() => <Daily />} />
-        <Route path="/stat" component={() => <Stat />} />
-        <Route path="/budget" component={() => "budget"} />
-        <Route path="/profile" component={() => "profile"} />
-
-        <BottomNavbar
-          defaultPath={"/"}
-          leftNav={[
-            {
-              icon: { name: "Calendar", size: 15, color: color.grayDarker2 },
-              text: "Daily",
-              pathname: "/"
-            },
-            {
-              icon: { name: "BarChart2", size: 15, color: color.grayDarker2 },
-              text: "Stat",
-              pathname: "/stat"
-            }
-          ]}
-          rightNav={[
-            {
-              icon: { name: "Book", size: 15, color: color.grayDarker2 },
-              text: "Budget",
-              pathname: "/budget"
-            },
-            {
-              icon: { name: "User", size: 15, color: color.grayDarker2 },
-              text: "Profile",
-              pathname: "/profile"
-            }
-          ]}
-        />
-      </Base>
+        </Base>
+      </DateContext.Provider>
     </Router>
   );
 }
--- src/pages/Daily.js
+++ src/pages/Daily.js
@@ -1,69 +1,86 @@
+// @flow
+
+import dayjs from "dayjs";
 import React from "react";
 import FlexWrapper from "../components/FlexWrapper";
-import { ListGroup, ListGroupItem } from "../components/ListGroup";
 import HeadingText from "../components/HeadingText";
 import HelperText from "../components/HelperText";
+import { ListGroup, ListGroupItem } from "../components/ListGroup";
+import DateContext from "../contexts/DateContext";
+import dummy from "../server/dummy.json";
 
 const Daily = () => {
   return (
-    <div
-      style={{
-        flex: 1,
-        padding: "8px 0 24px 0",
-        overflowY: "scroll",
-        zIndex: 1
-      }}
-    >
-      <FlexWrapper>
-        <ListGroup>
-          {[0, 0, 0, 0, 0].map((item, index) => (
-            <ListGroupItem key={index}>
-              <img
-                src="https://via.placeholder.com/150"
-                alt="et-lgi"
-                style={{
-                  height: 35,
-                  width: 35,
-                  objectFit: "cover",
-                  borderRadius: 35
-                }}
-              />
-              <div
-                style={{
-                  flex: 1,
-                  margin: "0 0 0 16px",
-                  paddingBottom: 16,
-                  borderBottom: "1px solid #a7a9aa"
-                }}
-              >
-                <FlexWrapper justifyContent={"space-between"} noPadding>
-                  <HeadingText type={"h5"}>Fathur Hidayat</HeadingText>
-                  <HeadingText type={"h5"} color={"#6def6d"}>
-                    $340.40
-                  </HeadingText>
-                </FlexWrapper>
-                <HelperText>Fri, 10AM</HelperText>
-              </div>
-            </ListGroupItem>
-          ))}
+    <DateContext.Consumer>
+      {({ etDate }) => (
+        <div
+          style={{
+            flex: 1,
+            padding: "8px 0 24px 0",
+            overflowY: "scroll",
+            zIndex: 1
+          }}
+        >
+          <FlexWrapper>
+            <ListGroup>
+              {dummy.data
+                .filter(
+                  item =>
+                    dayjs(item.date).format("DD-MM-YYYY") ===
+                      dayjs(etDate).format("DD-MM-YYYY") && item
+                )
+                .map((item, index) => (
+                  <ListGroupItem key={index}>
+                    <img
+                      src="https://via.placeholder.com/150"
+                      alt="et-lgi"
+                      style={{
+                        height: 35,
+                        width: 35,
+                        objectFit: "cover",
+                        borderRadius: 35
+                      }}
+                    />
+                    <div
+                      style={{
+                        flex: 1,
+                        margin: "0 0 0 16px",
+                        paddingBottom: 16,
+                        borderBottom: "1px solid #a7a9aa"
+                      }}
+                    >
+                      <FlexWrapper justifyContent={"space-between"} noPadding>
+                        <HeadingText type={"h5"}>{item.name}</HeadingText>
+                        <HeadingText type={"h5"} color={"#6def6d"}>
+                          ${item.amount}
+                        </HeadingText>
+                      </FlexWrapper>
+                      <HelperText>
+                        {dayjs(item.date).format("ddd, hA")}
+                      </HelperText>
+                    </div>
+                  </ListGroupItem>
+                ))}
 
-          <ListGroupItem key={99}>
-            <div
-              style={{
-                flex: 1,
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                marginLeft: 51
-              }}
-            >
-              <HelperText>Total</HelperText>
-              <HeadingText type={"h4"}>$340.40</HeadingText>
-            </div>
-          </ListGroupItem>
-        </ListGroup>
-      </FlexWrapper>
-    </div>
+              <ListGroupItem key={99}>
+                <div
+                  style={{
+                    flex: 1,
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginLeft: 51
+                  }}
+                >
+                  <HelperText>Total</HelperText>
+                  <HeadingText type={"h4"}>$340.40</HeadingText>
+                </div>
+              </ListGroupItem>
+            </ListGroup>
+          </FlexWrapper>
+        </div>
+      )}
+    </DateContext.Consumer>
   );
 };
 
