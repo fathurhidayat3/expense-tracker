6e311ff73a111f86467fd7dd052be6eeab4858dd
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-24 02:11:49 +0700

    Add AddTransaction2 page

---
+++ src/pages/AddTransaction2.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const AddTransaction2 = () => {
+  return (
+    <div
+      style={{
+        flex: 1,
+        display: "flex",
+        flexDirection: "column",
+        padding: "24px",
+        overflowY: "scroll",
+        zIndex: 1
+      }}
+    >
+      Add transaction
+    </div>
+  );
+};
+
+export default AddTransaction2;
