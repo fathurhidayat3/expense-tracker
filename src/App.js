d9092abe0842e5deca97f87d8948b50aad7f796c
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-24 02:12:59 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,9 +10,13 @@ import DateScroller from "./components/DateScroller";
 import HeadingText from "./components/HeadingText";
 import TopNavbar from "./components/TopNavbar/style";
 import { color } from "./constants";
+import DataContext from "./contexts/DataContext";
 import DateContext from "./contexts/DateContext";
+import AddTransaction1 from "./pages/AddTransaction1";
+import AddTransaction2 from "./pages/AddTransaction2";
 import Daily from "./pages/Daily";
 import Stat from "./pages/Stat";
+import dummy from "./server/dummy.json";
 
 function App() {
   const [etDate, setEtDate] = React.useState(dayjs().format("YYYY-MM-DD"));
@@ -24,73 +28,88 @@ function App() {
       };
     })
   );
+  const leftBottomNav = [
+    {
+      icon: {
+        name: "Calendar",
+        size: 15,
+        color: color.grayDarker2
+      },
+      text: "Daily",
+      pathname: "/"
+    },
+    {
+      icon: {
+        name: "BarChart2",
+        size: 15,
+        color: color.grayDarker2
+      },
+      text: "Stat",
+      pathname: "/stat"
+    }
+  ];
+  const rightBottomNav = [
+    {
+      icon: { name: "Book", size: 15, color: color.grayDarker2 },
+      text: "Budget",
+      pathname: "/budget"
+    },
+    {
+      icon: { name: "User", size: 15, color: color.grayDarker2 },
+      text: "Profile",
+      pathname: "/profile"
+    }
+  ];
 
   return (
     <Router>
-      <DateContext.Provider value={{ etDate: etDate, setEtDate: setEtDate }}>
-        <Base>
-          <TopNavbar>
-            <div style={{ display: "flex", justifyContent: "space-between" }}>
-              <HeadingText type="h3">Daily transaction</HeadingText>
+      <DataContext.Provider value={{ data: dummy.data }}>
+        <DateContext.Provider value={{ etDate: etDate, setEtDate: setEtDate }}>
+          <Base>
+            <TopNavbar>
+              <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <HeadingText type="h3">Daily transaction</HeadingText>
 
-              <ButtonIcon
-                icon={{ name: "Search", size: 15, color: color.dark }}
+                <ButtonIcon
+                  icon={{ name: "Search", size: 15, color: color.dark }}
+                />
+              </div>
+
+              <DateScroller
+                defaultDate={("0" + etDate).slice(-2)}
+                data={scrollerData}
+                buttonStyle={{
+                  activeBackground: "red",
+                  activeColor: "yellow"
+                }}
+                onButtonClick={data =>
+                  setEtDate(etDate.substr(0, 8) + data.num)
+                }
               />
-            </div>
+            </TopNavbar>
 
-            <DateScroller
-              defaultDate={("0" + etDate).slice(-2)}
-              data={scrollerData}
-              buttonStyle={{
-                activeBackground: "red",
-                activeColor: "yellow"
-              }}
-              onButtonClick={data => setEtDate(etDate.substr(0, 8) + data.num)}
+            <Route exact path="/" component={() => <Daily />} />
+            <Route path="/stat" component={() => <Stat />} />
+            <Route path="/budget" component={() => "budget"} />
+            <Route path="/profile" component={() => "profile"} />
+            <Route
+              path="/add-transaction"
+              component={() => <AddTransaction1 />}
             />
-          </TopNavbar>
+            <Route
+              path="/add-transaction-2"
+              component={() => <AddTransaction2 />}
+            />
 
-          <Route exact path="/" component={() => <Daily />} />
-          <Route path="/stat" component={() => <Stat />} />
-          <Route path="/budget" component={() => "budget"} />
-          <Route path="/profile" component={() => "profile"} />
-
-          <BottomNavbar
-            defaultPath={"/"}
-            leftNav={[
-              {
-                icon: {
-                  name: "Calendar",
-                  size: 15,
-                  color: color.grayDarker2
-                },
-                text: "Daily",
-                pathname: "/"
-              },
-              {
-                icon: {
-                  name: "BarChart2",
-                  size: 15,
-                  color: color.grayDarker2
-                },
-                text: "Stat",
-                pathname: "/stat"
-              }
-            ]}
-            rightNav={[
-              {
-                icon: { name: "Book", size: 15, color: color.grayDarker2 },
-                text: "Budget",
-                pathname: "/budget"
-              },
-              {
-                icon: { name: "User", size: 15, color: color.grayDarker2 },
-                text: "Profile",
-                pathname: "/profile"
-              }
-            ]}
-          />
-        </Base>
-      </DateContext.Provider>
+            <BottomNavbar
+              defaultPath={"/"}
+              leftNav={leftBottomNav}
+              rightNav={rightBottomNav}
+              buttonFloatingClick={"add-transaction"}
+            />
+          </Base>
+        </DateContext.Provider>
+      </DataContext.Provider>
     </Router>
   );
 }
