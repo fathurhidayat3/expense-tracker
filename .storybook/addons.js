f7be5d4850d28529fa6a9cdaa3a8782a5cbc2445
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-24 06:40:28 +0700

    Add storyshot

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,2 +1,5 @@
-import '@storybook/addon-actions/register';
-import '@storybook/addon-links/register';
+import "@storybook/addon-actions/register";
+import "@storybook/addon-links/register";
+import initStoryshots from "@storybook/addon-storyshots";
+
+initStoryshots();
