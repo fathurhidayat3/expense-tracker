23d7c5fbc3886b6f2831e581d721cdd6696245d2
fathurhidayat3 <fathur.hidayat@gmail.com>
2019-07-24 03:21:18 +0700

    Add transaction form skeleton

--- src/pages/AddTransaction2.js
+++ src/pages/AddTransaction2.js
@@ -1,4 +1,15 @@
 import React from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import HeadingText from "../components/HeadingText";
+
+const TransactionSchema = Yup.object().shape({
+  type: Yup.string().required("Required"),
+  info: Yup.string().required("Required"),
+  category: Yup.string().required("Required"),
+  date: Yup.string().required("Required"),
+  amount: Yup.string().required("Required")
+});
 
 const AddTransaction2 = () => {
   return (
@@ -12,7 +23,104 @@ const AddTransaction2 = () => {
         zIndex: 1
       }}
     >
-      Add transaction
+      {/* <HeadingText type={"h2"}>Add income transaction</HeadingText> */}
+
+      <Formik
+        initialValues={{
+          type: "",
+          info: "",
+          category: "",
+          date: "",
+          amount: ""
+        }}
+        validationSchema={TransactionSchema}
+        onSubmit={values => {
+          console.log(values);
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                margin: "4px 0",
+                padding: "4px 0"
+              }}
+            >
+              <HeadingText type={"h5"}>Select type</HeadingText>
+              <Field name="type" component="select">
+                <option value="income">Income</option>
+                <option value="expense">Expense</option>
+              </Field>
+              <div style={{ margin: "4px 0 0 0" }}>
+                {errors.type && touched.type ? <div>{errors.type}</div> : null}
+              </div>
+            </div>
+
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                margin: "4px 0",
+                padding: "4px 0"
+              }}
+            >
+              <HeadingText type={"h5"}>Add info</HeadingText>
+              <Field name="info" />
+              {errors.info && touched.info ? <div>{errors.info}</div> : null}
+            </div>
+
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                margin: "4px 0",
+                padding: "4px 0"
+              }}
+            >
+              <HeadingText type={"h5"}>Choose category</HeadingText>
+              <Field name="category" component="select">
+                <option value="income">Salary</option>
+                <option value="expense">Bank</option>
+              </Field>{" "}
+              {errors.category && touched.category ? (
+                <div>{errors.category}</div>
+              ) : null}
+            </div>
+
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                margin: "4px 0",
+                padding: "4px 0"
+              }}
+            >
+              <HeadingText type={"h5"}>Choose date</HeadingText>
+              <Field name="date" />
+              {errors.date && touched.date ? <div>{errors.date}</div> : null}
+            </div>
+
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                margin: "4px 0",
+                padding: "4px 0"
+              }}
+            >
+              <HeadingText type={"h5"}>Fill in amount</HeadingText>
+              <Field name="amount" />
+              {errors.amount && touched.amount ? (
+                <div>{errors.amount}</div>
+              ) : null}
+            </div>
+
+            <button type="submit">Submit</button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
